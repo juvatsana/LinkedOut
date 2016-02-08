90fb3672aa91c9b2fae680c85306e1446cf52f31
Peroumalle <mperouma@gmail.com>
2016-02-08 23:57:01 +0100

    Inscription.xhtml and inscriptionBean added

--- src/java/fr/umlv/inscription/InscriptionBean.java
+++ src/java/fr/umlv/inscription/InscriptionBean.java
@@ -5,8 +5,14 @@
  */
 package fr.umlv.inscription;
 
+import fr.umlv.login.DataConnect;
+import fr.umlv.login.LoginBean;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import javax.faces.FacesException;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 /**
@@ -16,20 +22,90 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "inscriptionBean")
 @ApplicationScoped
 public class InscriptionBean implements Serializable{
-    static class Formation {
-        private String degree;
-        private String school;
-        private int year;
-        public Formation(String degree,String school,int year) {
-            this.degree = degree;
-            this.school = school;
-            this.year = year;
-        }
-    }
-    private static final long serialVersionUID = 1L;
-    private ArrayList<Formation> formations = new ArrayList<>();
-    private String user;
+
+    Connection connexion;
+    Statement statement;
+    ResultSet resultSet;
+    private String username;
     private String pass;
-    private String logUser;
-    private String logPass;
+    private String firstname;
+    private String surname;
+    private int age;
+    private int tel;
+    private String adress;
+    
+    private String error;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    
+    public boolean saveUser() { //TODO ajouter l'id de l'user
+        String request = "INSERT INTO `User`(`username`, `password`, `firstname`, `surname`, `age`, `telephone`, `adresse`) VALUES ('"+username+"','"+ pass+"','"+firstname+"','"+surname+"','"+age+"','"+tel+"','"+ adress+"');";
+        try {
+            connexion = DataConnect.getConnection();
+            statement = connexion.createStatement();
+            statement.execute(request);
+        } catch (Exception e) {
+            System.err.println("Error save Formation " + request+"\n"+e);
+            if(e.getMessage().toLowerCase().contains("Duplicate entry".toLowerCase())){
+                error = "L'utilisateur "+username+" existe déjà";
+            }
+            return false;
+        }
+        return true;
+        
+    }
 }
