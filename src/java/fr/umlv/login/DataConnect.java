d7f8ec53b39b653f0e8bfed0fcfe51726bf52700
juvatsana <julienvorabout@hotmail.fr>
2016-02-10 15:32:22 +0100

    Add research CV page

--- src/java/fr/umlv/login/DataConnect.java
+++ src/java/fr/umlv/login/DataConnect.java
@@ -9,24 +9,23 @@ package fr.umlv.login;
  *
  * @author Vatsana
  */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
 public class DataConnect {
- 
+
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ProjetJEE","root", "root");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ProjetJEE", "root", "root");
             return con;
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-            System.err.println("Database.getConnection() Error -->"+ex+ ex.getMessage());
+            System.err.println("Database.getConnection() Error -->" + ex + ex.getMessage());
             return null;
         }
     }
- 
+
     public static void close(Connection con) {
         try {
             con.close();
